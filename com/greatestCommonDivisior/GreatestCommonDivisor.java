f5d120e90e700c26d45c54619c39568128732008
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:42:46 +0530

    Update GreatestCommonDivisor.java

--- com/greatestCommonDivisior/GreatestCommonDivisor.java
+++ com/greatestCommonDivisior/GreatestCommonDivisor.java
@@ -1,2 +1,26 @@
-package com.reshnu.com.greatestCommonDivisior;public class GreatestCommonDivisor {
+package com.reshnu.com.greatestCommonDivisior;
+
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+public static void main(String [] args){
+
+    Scanner d=new Scanner(System.in);
+    int num1=d.nextInt();
+    int num2=d.nextInt();
+    int gcd=1;
+
+    for(int i=1;i<=num1 && i<=num2;++i){
+
+        if(num1%i==0 && num2%i==0){
+
+            gcd=i;
+
+        }
+
+    }
+    System.out.println(gcd);
 }
+
+}
+
