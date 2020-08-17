a09fcdd78f3c8adfcbd3b632f4d64fb4b9a24ea5
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:41:55 +0530

    Update GreatestAmong10Numbers.java

--- com/greatestAmong10Numbers/GreatestAmong10Numbers.java
+++ com/greatestAmong10Numbers/GreatestAmong10Numbers.java
@@ -1,2 +1,39 @@
-package com.reshnu.com.greatestAmong10Numbers;public class GreatestAmong10Numbers {
+package com.reshnu.com.greatestAmong10Numbers;
+import java.util.Scanner;
+
+public class GreatestAmong10Numbers {
+    int [] a=new int[10];
+
+    void readNumbers(){
+       for(int i=0;i<10;i++){
+
+           Scanner d=new Scanner(System.in);
+           a[i]=d.nextInt();
+
+       }
+
+    }
+
+    int findGreatestNumber(){
+        int greaterNumber=a[0];
+        for (int i=0;i<10;i++){
+
+                if(a[i]>greaterNumber){
+
+                    greaterNumber=a[i];
+
+
+
+                }
+
+        }
+        return  greaterNumber;
+    }
+    public static void main(String [] args){
+        GreatestAmong10Numbers obj=new GreatestAmong10Numbers();
+        obj.readNumbers();
+        int num=obj.findGreatestNumber();
+        System.out.print(num);
+    }
+
 }
