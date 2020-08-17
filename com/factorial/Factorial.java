61af6ac8d946ea5b248896e58d17d8322887ce0e
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:40:27 +0530

    Update Factorial.java

--- com/factorial/Factorial.java
+++ com/factorial/Factorial.java
@@ -1,2 +1,21 @@
-package com.reshnu.com.factorial;public class Factorial {
+package com.reshnu.com.factorial;
+
+class Factorial {
+
+    void factorial(int num){
+        int fact=1;
+        for(int i=1;i<=num;i++){
+
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
+
+class MainClass{
+    public static void main(String[] args){
+        Factorial fact=new Factorial();
+        fact.factorial(5);
+
+    }
 }
