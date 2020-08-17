b9f433fc24df6ac6fd5f71a2c4d1662e1a1514ca
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:45:40 +0530

    Update ReverseANumberUsingReccursion.java

--- com/reverseANumber/ReverseANumberUsingReccursion.java
+++ com/reverseANumber/ReverseANumberUsingReccursion.java
@@ -1,2 +1,47 @@
-package com.reshnu.com.reverseANumber;public class ReverseANumberUsingReccursion {
+package com.reshnu.com.reverseANumber;
+
+import java.util.Scanner;
+
+public class ReverseAFloatNumber {
+
+    String[] splitNumber(float num){
+        String st=String.valueOf(num);
+        String [] str=st.split("\\.");
+
+
+        return str;
+
+    }
+    void reverseNumber(String[] ans){
+        int rev1=0;
+        int rev2=0;
+        int before=Integer.parseInt(ans[0]);
+        int after=Integer.parseInt(ans[1]);
+
+        while (before!=0){
+            int rem=before%10;
+            rev1=rev1*10+rem;
+            before=before/10;
+
+
+        }
+        while (after!=0){
+
+            int rem=after%10;
+            rev2=rev2*10+rem;
+            after=after/10;
+        }
+        System.out.println(rev1+"."+rev2);
+
+    }
+
+    public static void main(String [] args){
+
+        Scanner d=new Scanner(System.in);
+        float num=d.nextFloat();
+        ReverseAFloatNumber obj=new ReverseAFloatNumber();
+        String ans[]=obj.splitNumber(num);
+        obj.reverseNumber(ans);
+
+    }
 }
