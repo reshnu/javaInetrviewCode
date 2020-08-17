40a435d418bfaa869da740e55d10ee9594962c8d
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:43:59 +0530

    Update Palindrome.java

--- com/palindrome/Palindrome.java
+++ com/palindrome/Palindrome.java
@@ -1,2 +1,28 @@
-package com.reshnu.com.palindrome;public class Palindrome {
+package com.reshnu.com.palindrome;
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args){
+
+        Scanner d =new Scanner(System.in);
+        int num=d.nextInt();
+        int rev=0;
+        int temp=num;
+
+        for(;num!=0;){
+
+            int rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        if(rev==temp){
+            System.out.print("given number is palindrome");
+        }
+        else{
+            System.out.print("given number not a palindrome");
+        }
+
+    }
+
 }
