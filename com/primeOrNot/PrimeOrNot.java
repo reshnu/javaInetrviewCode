5b34679fad7449fd90dac55509eadd4fe6e964b5
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:44:30 +0530

    Update PrimeOrNot.java

--- com/primeOrNot/PrimeOrNot.java
+++ com/primeOrNot/PrimeOrNot.java
@@ -1,2 +1,19 @@
-package com.reshnu.com.primeOrNot;public class PrimeOrNot {
-}
+package com.reshnu.com.primeOrNot;
+import java.util.Scanner;
+
+public class PrimeOrNot {
+
+    public static void main(String[] args){
+        Scanner d=new Scanner(System.in);
+        int flag =1;
+
+        System.out.println("Enter a number check whether prime or not");
+        int num1=d.nextInt();
+        for(int i=2;i<num1/2;i++){
+
+
+
+        }
+
+    }
+ }
