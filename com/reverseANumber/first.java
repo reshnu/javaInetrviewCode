9caae106269925d8d16b9d7ab5996c391b221962
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:46:13 +0530

    Update first.java

--- com/reverseANumber/first.java
+++ com/reverseANumber/first.java
@@ -1,2 +1,20 @@
-package com.reshnu.com.reverseANumber;public class first {
+package com.reshnu.com.reverseANumber;
+
+public class ReverseANumberUsingReccursion {
+
+    void recursion(int num){
+        if(num<10){
+            System.out.println(num);
+
+        }else{
+            System.out.print(num % 10);
+            recursion(num/10);
+        }
+
+    }
+    public static void main(String [] args){
+        ReverseANumberUsingReccursion obj=new ReverseANumberUsingReccursion();
+        obj.recursion(521);
+
+    }
 }
