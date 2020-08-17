40635ae95e0d52866cd3b844a7dc461409bd81e3
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:41:07 +0530

    Update Fibonacci.java

--- com/fibonacci/Fibonacci.java
+++ com/fibonacci/Fibonacci.java
@@ -1,2 +1,31 @@
-package com.reshnu.com.fibonacci;public class Fibonacci {
+package com.reshnu.com.fibonacci;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+
+    void fib(){
+        Scanner d=new Scanner(System.in);
+        int a=0,b=1;
+        int c=0;
+        System.out.println("Enter the limit");
+
+        int limit=d.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        for(int i=0;i<limit;i++){
+
+            c=a+b;
+            a=b;
+            b=c;
+            System.out.println(+c);
+        }
+
+
+    }
+    public static void main(String[] args){
+        Fibonacci obj=new Fibonacci();
+        obj.fib();
+
+    }
 }
