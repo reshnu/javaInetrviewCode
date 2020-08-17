56b0db888e99adeae7ed0cc045948d4777e7ee08
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:45:12 +0530

    Update ReverseAFloatNumber.java

--- com/reverseANumber/ReverseAFloatNumber.java
+++ com/reverseANumber/ReverseAFloatNumber.java
@@ -1,2 +1,24 @@
-package com.reshnu.com.reverseANumber;public class ReverseAFloatNumber {
+package com.reshnu.com.reverseANumber;
+
+import java.util.Scanner;
+
+public class first {
+     int reverseANumber(int num){
+        int rev = 0;
+
+        while (num!=0) {
+            int remainder = num % 10;
+            rev = rev * 10 + remainder;
+            num = num / 10;
+        }
+        return rev;
+    }
+    public static void main(String [] args){
+        Scanner d= new Scanner(System.in);
+        int num=d.nextInt();
+        first obj=new first();
+        int rev=obj.reverseANumber(num);
+        System.out.println(rev);
+
+    }
 }
