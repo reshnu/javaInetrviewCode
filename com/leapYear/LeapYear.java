6cb588190421ab72402faff03e62bed167926338
Reshnu Chandran G B <47183757+reshnu@users.noreply.github.com>
2020-08-17 10:43:27 +0530

    Update LeapYear.java

--- com/leapYear/LeapYear.java
+++ com/leapYear/LeapYear.java
@@ -1,2 +1,30 @@
-package com.reshnu.com.leapYear;public class LeapYear {
+package com.reshnu.com.leapYear;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String [] args){
+
+        Scanner d =new Scanner(System.in);
+        int year=d.nextInt();
+
+        if(year%4==0){
+            if (year%100==0){
+                if (year%400==0){
+
+                    System.out.print("Given Number is LeapYear");
+                }
+                else{
+
+                    System.out.print("given number  a palidrome");
+                }
+                System.out.print("given number is not a palindrome");
+
+            }
+
+        }
+
+    }
+
 }
